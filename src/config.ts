347c884711de414f7233cb26993d520962e59a2e
Ayush Sonawale <75654165+ayushsonawale@users.noreply.github.com>
2025-10-29 10:15:57 +0530

    changes

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = 'https://second-brain-backend-git-main-ayushsonawales-projects.vercel.app/'
+export const BACKEND_URL = 'https://second-brain-backend-git-main-ayushsonawales-projects.vercel.app'
